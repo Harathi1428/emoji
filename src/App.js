802de0c2c314d4fc20eed513f35fc2e8a59ea10d
Harathi1428 <harathiyenni@gmail.com>
2023-11-28 18:46:20 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import EmojiData from "./EmojiData.js";
 import './App.css';
+import {useState,useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const [search,setSearch]=useState("");
+const [data,setData]=useState([]);
+  function update(event){
+setSearch(event.target.value);
+  }  
+  useEffect(()=>{
+const data=EmojiData.filter(emoji => emoji.name.toLowerCase().includes(search.toLowerCase()));
+setData(data);
+  },[search])
+  return (
+   <div className="outer">
+    <center>
+      <h1 className="emoji-search">emoji search</h1>
+      <input type="text" name="search" value={search} onChange={update}/>
+    </center>
+    {data.map(emoji=>
+    <div class="card">
+      <div class="card-body">
+        {emoji.symbol} {emoji.name}
+      </div>
+    </div>
+    )}
+   </div>
+  ); 
+    }
+export default App
